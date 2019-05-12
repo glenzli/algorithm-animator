eb5b09af7fab52681d5bb74a788b6a1e4ac369ac
luz-alphacode <luz_alphacode@outlook.com>
2019-05-13 01:04:03 +0800

    remove doc & external deps

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   publicPath: './',
-  productionSourceMap: false
+  productionSourceMap: false,
+  filenameHashing: false,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.externals = {
+        paper: 'paper',
+        vue: 'Vue'
+      }
+    }
+  }
 }
 
